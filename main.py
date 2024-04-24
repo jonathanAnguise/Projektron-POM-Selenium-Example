ff0ec9ca2b7d63c358040b74a827e45af29c6e37
jonathanAnguise <jonathan.anguise@protonmail.com>
2024-04-24 23:22:10 +0200

    use model, this commit need to be removed or ammended

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from dotenv import dotenv_values
+from pages.login_page import LoginPage
+from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
 
 def main():
    my_secret = dotenv_values(".env")
    print(my_secret)
+   driver = webdriver.Chrome()
+
+   my_page=LoginPage(driver)
+   my_page.open("")
+   my_page2=BasePage(driver)
+   input()
+   my_page2.open("")
+   input()
 
 if __name__ == "__main__":
    main()
