c6457d4c1ce60749becfce755d112e0ad5b43371
jonathanAnguise <jonathan.anguise@protonmail.com>
2024-04-28 17:05:33 +0200

    main: Add main actions for connecting step

--- main.py
+++ main.py
@@ -9,13 +9,11 @@ def main():
    my_secret = dotenv_values(".env")
    print(my_secret)
    driver = webdriver.Chrome()
+   login_page = LoginPage(driver)
+   login_page.open()
+   login_page.enter_email(my_secret.get("USERNAME"))
+   input()
 
-   my_page=LoginPage(driver)
-   my_page.open("")
-   my_page2=BasePage(driver)
-   input()
-   my_page2.open("")
-   input()
 
 if __name__ == "__main__":
    main()
