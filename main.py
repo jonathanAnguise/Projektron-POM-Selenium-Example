4c0f1c48057b71216fcf04fae212f46a8622ef4d
jonathanAnguise <jonathan.anguise@protonmail.com>
2024-05-04 22:56:43 +0200

    main: Add 2 new functions
    Functions added:
    * parse_time_string()
    * validate_resting_budget_is_enough()

--- main.py
+++ main.py
@@ -20,10 +20,50 @@ Example:
         $ python web_automation.py
 """
 
+from time import strptime, mktime
 from selenium import webdriver
 from dotenv import dotenv_values
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
+
+
+def parse_time_string(time_string) -> float:
+    """
+    This function is taking a string time and return a float in seconds
+
+    Arguments:
+        string: time string in the format "xd xx:xxh" or "xx:xx"
+    Return:
+        float corresponding of the time interval in second
+    """
+    one_day_in_sec = 86400
+    if "d" in time_string and "h" in time_string:
+        parsed_time = (
+            mktime(strptime(time_string, "%dd %H:%Mh")) + one_day_in_sec
+        )
+
+    else:
+        parsed_time = mktime(strptime(time_string, "%H:%Mh"))
+    return parsed_time - mktime(
+        strptime("1900, 1, 1, 0, 0", "%Y, %m, %d, %H, %M")
+    )
+
+
+def validate_resting_budget_is_enough(
+    unrecorded_effort, duration_time, budget_time
+) -> bool:
+    """
+    Validates if the resting budget is enough for the given duration and unrecorded effort.
+
+    Args:
+        unrecorded_effort (struct_time): The unrecorded effort time.
+        duration_time (struct_time): The duration time.
+        budget_time (struct_time): The resting budget time.
+
+    Returns:
+        bool: True if the resting budget is enough, False otherwise.
+    """
+    return unrecorded_effort + duration_time <= budget_time
 
 
 def main():
@@ -58,4 +98,12 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    budget = parse_time_string("2d 04:00h")
+    duration = parse_time_string("2d 02:00h")
+    unrec = parse_time_string("02:00h")
+    print(f"{budget=}, {duration=}, {unrec=}")
+    print(
+        validate_resting_budget_is_enough(
+            unrecorded_effort=unrec, duration_time=duration, budget_time=budget
+        )
+    )
